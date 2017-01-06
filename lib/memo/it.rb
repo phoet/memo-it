91f228fa8b4c79ba54027a30a62cba1d70360952
phoet <phoetmail@googlemail.com>
2017-01-06 13:52:29 +0100

    actually return something so it memoizes and use mocks for expectations

--- lib/memo/it.rb
+++ lib/memo/it.rb
@@ -1,12 +1,13 @@
 module Memo
-  VERSION = '0.1.1'
+  VERSION = '0.1.2'
   module It
     def memo(&block)
       keys = block.source_location
-      keys << block.binding.local_variables.map { |name| [name, block.binding.local_variable_get(name).to_s] }
+      keys << block.binding.local_variables.map { |name| [name, block.binding.local_variable_get(name)] }
+      keys = keys.flatten.map(&:to_s)
 
       @_memo_it ||= {}
-      @_memo_it[keys] if @_memo_it.key? keys
+      return @_memo_it[keys] if @_memo_it.key? keys
       @_memo_it[keys] = yield
     end
   end
--- test/memo/it_test.rb
+++ test/memo/it_test.rb
@@ -2,31 +2,49 @@ require 'test_helper'
 
 module Memo
   class ItTest < Minitest::Test
+    def setup
+      @mock = MiniTest::Mock.new
+    end
+
     def test_memoizes_stuff
-      assert memo_without_parameters == :stuff
+      @mock.expect(:slow, :stuff)
+      10.times { assert memo_without_parameters == :stuff }
+      @mock.verify
+    end
+
+    def test_memoizes_nil
+      @mock.expect(:slow, nil)
+      10.times { assert memo_without_parameters.nil? }
+      @mock.verify
     end
 
     def test_memoizes_stuff_with_parameters
-      assert memo_with_parameters == :stuff_with_parameters_1_2
-      assert memo_with_parameters(3, 4) == :stuff_with_parameters_3_4
+      @mock.expect(:slow, :stuff12, [1, 2])
+      10.times { assert memo_with_parameters == :stuff12 }
+      @mock.expect(:slow, :stuff34, [3, 4])
+      10.times { assert memo_with_parameters(3, 4) == :stuff34 }
+      @mock.verify
     end
 
     def test_memoizes_without_leaking
       10.times do |i|
         j = rand
-        assert memo_with_parameters(i, j) == :"stuff_with_parameters_#{i}_#{j}"
+        ret_val = :"stuff#{i}#{j}"
+        @mock.expect(:slow, ret_val, [i, j])
+        10.times { assert memo_with_parameters(i, j) == ret_val }
       end
+      @mock.verify
     end
 
     def memo_without_parameters
       memo do
-        :stuff
+        @mock.slow
       end
     end
 
     def memo_with_parameters(some = 1, parameters = 2)
       memo do
-        :"stuff_with_parameters_#{some}_#{parameters}"
+        @mock.slow(some, parameters)
       end
     end
   end
